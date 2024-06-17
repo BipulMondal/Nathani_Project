fc4c4f6964525807842de5d642a11f5c5de7ac29
Bipul Mondal <111245325+BipulMondal@users.noreply.github.com>
2024-06-17 11:40:41 +0530

    Delete Backend_Nathani directory

--- Backend_Nathani/routes/v1/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const multer = require('multer');
-var storage = multer.memoryStorage()
-var upload = multer({ storage: storage });
-
-
-module.exports = router
