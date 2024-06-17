c4a0ab41acd3a3d109b9dd289ebd7d0468bba0da
Bipul Mondal <bipulmondal3007@gmail.com>
2024-06-17 07:31:48 +0530

    update new

---
+++ Backend_Nathani/routes/v1/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage });
+
+
+module.exports = router
